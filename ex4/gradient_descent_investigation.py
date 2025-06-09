7cac306102e50e2bde4f1acb21542309df8c1ba2
lenneh <lenneh@betterDebian.myguest.virtualbox.org>
2025-06-09 23:11:19 +0300

    added ex4

---
+++ ex4/gradient_descent_investigation.py
@@ -0,0 +1,163 @@
+import numpy as np
+import pandas as pd
+from typing import Tuple, List, Callable, Type, Any
+
+from base_module import BaseModule
+from base_learning_rate import  BaseLR
+from gradient_descent import GradientDescent
+from learning_rate import FixedLR
+
+
+
+# from IMLearn.desent_methods import GradientDescent, FixedLR, ExponentialLR
+from modules import L1, L2
+from logistic_regression import LogisticRegression
+from utils import split_train_test
+
+import plotly.graph_objects as go
+
+
+def plot_descent_path(module: Type[BaseModule],
+                      descent_path: np.ndarray,
+                      title: str = "",
+                      xrange=(-1.5, 1.5),
+                      yrange=(-1.5, 1.5)) -> go.Figure:
+    """
+    Plot the descent path of the gradient descent algorithm
+
+    Parameters:
+    -----------
+    module: Type[BaseModule]
+        Module type for which descent path is plotted
+
+    descent_path: np.ndarray of shape (n_iterations, 2)
+        Set of locations if 2D parameter space being the regularization path
+
+    title: str, default=""
+        Setting details to add to plot title
+
+    xrange: Tuple[float, float], default=(-1.5, 1.5)
+        Plot's x-axis range
+
+    yrange: Tuple[float, float], default=(-1.5, 1.5)
+        Plot's x-axis range
+
+    Return:
+    -------
+    fig: go.Figure
+        Plotly figure showing module's value in a grid of [xrange]x[yrange] over which regularization path is shown
+
+    Example:
+    --------
+    fig = plot_descent_path(IMLearn.desent_methods.modules.L1, np.ndarray([[1,1],[0,0]]))
+    fig.show()
+    """
+    def predict_(w):
+        return np.array([module(weights=wi).compute_output() for wi in w])
+
+    from utils import decision_surface
+    return go.Figure([decision_surface(predict_, xrange=xrange, yrange=yrange, density=70, showscale=False),
+                      go.Scatter(x=descent_path[:, 0], y=descent_path[:, 1], mode="markers+lines", marker_color="black")],
+                     layout=go.Layout(xaxis=dict(range=xrange),
+                                      yaxis=dict(range=yrange),
+                                      title=f"GD Descent Path {title}"))
+
+
+def get_gd_state_recorder_callback() -> Tuple[Callable[[GradientDescent, ...], None], List[np.ndarray], List[np.ndarray]]:
+    """
+    Callback generator for the GradientDescent class, recording the objective's value and parameters at each iteration
+
+    Return:
+    -------
+    callback: Callable[[GradientDescent, ...], None]
+        Callback function to be passed to the GradientDescent class, recording the objective's value and parameters
+        at each iteration of the algorithm
+
+    values: List[np.ndarray]
+        Recorded objective values
+
+    weights: List[np.ndarray]
+        Recorded parameters
+    """
+    values = []
+    weights = []
+
+    def callback(_: GradientDescent, **kwargs: Any):
+        values.append(kwargs["val"])
+        weights.append(kwargs["weights"].copy())
+
+    return callback, values, weights
+
+def compare_fixed_learning_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
+                                 etas: Tuple[float] = (1, .1, .01, .001)):
+    for eta in etas:
+        lr = FixedLR(eta)
+        for module_cls in [L1, L2]:
+
+            # initialize module
+            module = module_cls(weights=init.copy())
+            module_name = module_cls.__name__
+            callback, values, weights = get_gd_state_recorder_callback()
+
+            # run gradient decent
+            gd = GradientDescent(lr, tol=1e-5, max_iter=1000, out_type='last', callback=callback)
+            gd.fit(module, np.array([]), np.array([])) # X, y are ignored in L1, L2
+
+            # convert weights to ndarray for plot
+            descent_path = np.array(weights)
+            # Plot the descent path
+            fig = plot_descent_path(module_cls, descent_path,
+                                    title=f"{module_name} with eta={eta}")
+            fig.show()
+
+
+
+
+def load_data(path: str = "SAheart.data", train_portion: float = .8) -> \
+        Tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
+    """
+    Load South-Africa Heart Disease dataset and randomly split into a train- and test portion
+
+    Parameters:
+    -----------
+    path: str, default= "../datasets/SAheart.data"
+        Path to dataset
+
+    train_portion: float, default=0.8
+        Portion of dataset to use as a training set
+
+    Return:
+    -------
+    train_X : DataFrame of shape (ceil(train_proportion * n_samples), n_features)
+        Design matrix of train set
+
+    train_y : Series of shape (ceil(train_proportion * n_samples), )
+        Responses of training samples
+
+    test_X : DataFrame of shape (floor((1-train_proportion) * n_samples), n_features)
+        Design matrix of test set
+
+    test_y : Series of shape (floor((1-train_proportion) * n_samples), )
+        Responses of test samples
+    """
+    df = pd.read_csv(path)
+    df.famhist = (df.famhist == 'Present').astype(int)
+    return split_train_test(df.drop(['chd', 'row.names'], axis=1), df.chd, train_portion)
+
+
+def fit_logistic_regression():
+    # Load and split SA Heard Disease dataset
+    X_train, y_train, X_test, y_test = load_data()
+
+    # Plotting convergence rate of logistic regression over SA heart disease data
+    raise NotImplementedError()
+
+    # Fitting l1- and l2-regularized logistic regression models, using cross-validation to specify values
+    # of regularization parameter
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    compare_fixed_learning_rates()
+    fit_logistic_regression()
---
+++ ex4/modules.py
@@ -0,0 +1,288 @@
+import numpy as np
+from base_module import BaseModule
+
+
+class L2(BaseModule):
+    """
+    Class representing the L2 module
+
+    Represents the function: f(w)=||w||^2_2
+    """
+    def __init__(self, weights: np.ndarray = None):
+        """
+        Initialize a module instance
+
+        Parameters:
+        ----------
+        weights: np.ndarray, default=None
+            Initial value of weights
+        """
+        super().__init__(weights)
+
+    def compute_output(self, **kwargs) -> np.ndarray:
+        """
+        Compute the output value of the L2 function at point self.weights
+
+        Parameters
+        ----------
+        kwargs:
+            No additional arguments are expected
+
+        Returns
+        -------
+        output: ndarray of shape (1,)
+            Value of function at point self.weights
+        """
+        return np.array(np.linalg.norm(self.weights_, ord=2) ** 2)
+
+    def compute_jacobian(self, **kwargs) -> np.ndarray:
+        """
+        Compute L2 derivative with respect to self.weights at point self.weights
+
+        Parameters
+        ----------
+        kwargs:
+            No additional arguments are expected
+
+        Returns
+        -------
+        output: ndarray of shape (n_in,)
+            L2 derivative with respect to self.weights at point self.weights
+        """
+        return 2 * self.weights_
+
+
+
+class L1(BaseModule):
+    def __init__(self, weights: np.ndarray = None):
+        """
+        Initialize a module instance
+
+        Parameters:
+        ----------
+        weights: np.ndarray, default=None
+            Initial value of weights
+        """
+        super().__init__(weights)
+
+    def compute_output(self, **kwargs) -> np.ndarray:
+        """
+        Compute the output value of the L1 function at point self.weights
+
+        Parameters
+        ----------
+        kwargs:
+            No additional arguments are expected
+
+        Returns
+        -------
+        output: ndarray of shape (1,)
+            Value of function at point self.weights
+        """
+        return np.array(np.linalg.norm(self.weights_, ord=1) ** 2)
+
+    def compute_jacobian(self, **kwargs) -> np.ndarray:
+        """
+        Compute L1 derivative with respect to self.weights at point self.weights
+
+        Parameters
+        ----------
+        kwargs:
+            No additional arguments are expected
+
+        Returns
+        -------
+        output: ndarray of shape (n_in,)
+            L1 derivative with respect to self.weights at point self.weights
+        """
+        if self.weights_ == np.zeros(self.weights_.shape):
+            return np.linspace(-1, 1)
+        else:
+            return np.sign(self.weights_)
+
+
+class LogisticModule(BaseModule):
+    """
+    Class representing the logistic regression objective function
+
+    Represents the function: f(w) = - (1/m) sum_i^m[y_i*<x_i,w> - log(1+exp(<x_i,w>))]
+    """
+    def __init__(self, weights: np.ndarray = None):
+        """
+        Initialize a logistic regression module instance
+
+        Parameters:
+        ----------
+        weights: np.ndarray, default=None
+            Initial value of weights
+        """
+        super().__init__(weights)
+
+    def compute_output(self, X: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Compute the output value of the logistic regression objective function at point self.weights
+
+        Parameters
+        ----------
+        X: ndarray of shape (n_samples, n_features)
+            Design matrix to use when computing objective
+
+        y: ndarray of shape(n_samples,)
+            Binary labels of samples to use when computing objective
+
+        Returns
+        -------
+        output: ndarray of shape (1,)
+            Value of function at point self.weights
+        """
+        # f(w) = - (1/m) sum_i^m[y_i*<x_i,w> - log(1+exp(<x_i,w>))]
+        n_samples = y.shape[0]
+        reg_sum = 0
+
+        # iterate over samples size, apply inner part
+        # of the sum and sum
+        for i in range(n_samples):
+            sum_i = y[i] * np.inner(X[i], self.weights_) - \
+                    np.log(1 + np.exp(np.inner(X[i], self.weights_)))
+            reg_sum += sum_i
+
+        return np.array([-reg_sum / n_samples])
+
+
+    def compute_jacobian(self, X: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Compute the gradient of the logistic regression objective function at point self.weights
+
+        Parameters
+        ----------
+        X: ndarray of shape (n_samples, n_features)
+            Design matrix to use when computing objective
+
+        y: ndarray of shape(n_samples,)
+            Binary labels of samples to use when computing objective
+
+        Returns
+        -------
+        output: ndarray of shape (n_features,)
+            Derivative of function with respect to self.weights at point self.weights
+        """
+        # d/dw f(w) = -1/m sum_i^m[y_ix_i-(exp(<x_i,w>)/(1+exp(<x_i,w>))x_i]
+        n_samples = y.shape[0]
+        der_sum = np.ndarray([])
+
+        # iterate over sample size and apply
+        # inner sum
+        for i in range(n_samples):
+            sum_i = y[i] * X[i] - (np.exp(np.inner(X[i], self.weights_)) /
+                                   (1 + np.exp(np.inner(X[i], self.weights_))))
+            der_sum += sum_i
+
+        return -(der_sum / n_samples)
+
+
+class RegularizedModule(BaseModule):
+    """
+    Class representing a general regularized objective function of the format:
+                                    f(w) = F(w) + lambda*R(w)
+    for F(w) being some fidelity function, R(w) some regularization function and lambda
+    the regularization parameter
+    """
+    def __init__(self,
+                 fidelity_module: BaseModule,
+                 regularization_module: BaseModule,
+                 lam: float = 1.,
+                 weights: np.ndarray = None,
+                 include_intercept: bool = True):
+        """
+        Initialize a regularized objective module instance
+
+        Parameters:
+        -----------
+        fidelity_module: BaseModule
+            Module to be used as a fidelity term
+
+        regularization_module: BaseModule
+            Module to be used as a regularization term
+
+        lam: float, default=1
+            Value of regularization parameter
+
+        weights: np.ndarray, default=None
+            Initial value of weights
+
+        include_intercept: bool default=True
+            Should fidelity term (and not regularization term) include an intercept or not
+        """
+        super().__init__()
+        self.fidelity_module_, self.regularization_module_, self.lam_ = fidelity_module, regularization_module, lam
+        self.include_intercept_ = include_intercept
+
+        if weights is not None:
+            self.weights = weights
+
+    def compute_output(self, **kwargs) -> np.ndarray:
+        """
+        Compute the output value of the regularized objective function at point self.weights
+
+        Parameters
+        ----------
+        kwargs:
+            No additional arguments are expected
+
+        Returns
+        -------
+        output: ndarray of shape (1,)
+            Value of function at point self.weights
+        """
+        return self.fidelity_module_.compute_output(**kwargs) + self.lam_ * self.regularization_module_.compute_output(**kwargs)
+
+    def compute_jacobian(self, **kwargs) -> np.ndarray:
+        """
+        Compute module derivative with respect to self.weights at point self.weights
+
+        Parameters
+        ----------
+        kwargs:
+            No additional arguments are expected
+
+        Returns
+        -------
+        output: ndarray of shape (n_in,)
+            Derivative with respect to self.weights at point self.weights
+        """
+        fidelity_grad = self.fidelity_module_.compute_jacobian(**kwargs)
+        reg_grad = self.regularization_module_.compute_jacobian(**kwargs)
+
+        if self.include_intercept_:
+            # Add 0 for intercept term
+            reg_grad = np.concatenate(([0.], reg_grad))
+
+        return fidelity_grad + self.lam_ * reg_grad
+
+    @property
+    def weights(self):
+        """
+        Wrapper property to retrieve module parameter
+
+        Returns
+        -------
+        weights: ndarray of shape (n_in, n_out)
+        """
+        return self.weights_
+
+    @weights.setter
+    def weights(self, weights: np.ndarray) -> None:
+        """
+        Setter function for module parameters
+
+        In case self.include_intercept_ is set to True, weights[0] is regarded as the intercept
+        and is not passed to the regularization module
+
+        Parameters
+        ----------
+        weights: ndarray of shape (n_in, n_out)
+            Weights to set for module
+        """
+        self.weights_ = weights
+        self.fidelity_module_.weights = weights
+        self.regularization_module_.weights = weights[1:] if self.include_intercept_ else weights
